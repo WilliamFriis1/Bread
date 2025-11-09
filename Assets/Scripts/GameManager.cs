922fe31f6c380b43d81388f67a01c83ae7de8adb
HecticSwede <hectorastr@gmail.com>
2025-11-09 23:51:01 +0100

    Implemented logic for losing
    - Added logic for transitioning to win and lose screen in GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,27 +1,30 @@
 using UnityEngine;
 using UnityEngine.UI;
 using System;
+using System.Collections;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private Button nextGamePhase;
+    [SerializeField] private CanvasGroup m_gameScene;
+    [SerializeField] private CanvasGroup m_winScene;
+    [SerializeField] private CanvasGroup m_loseScene;
+    //[SerializeField] private CanvasGroup m_pauseScene;
     public enum GameDay
     {
-        Day0,
-        Day1, 
-        Day2,
-        Day3
+        Day0, Day1, Day2, Day3
     }
-
     public enum GamePhase
     {
-        RoundStart,
-        PlaceBet,
-        SpeakingToNPC,
-        RoundEnd
+        RoundStart, PlaceBet, SpeakingToNPC, RoundEnd
     }
+    public Player Player { get; set; }
+    public OddsManager OddsManager { get; set; }
+    public GamePhase Phase;
+    public GameDay Day;
 
     private static GameManager instance;
-
+    private FadeAnimator m_fadeAnimator;
     public static GameManager Instance
     {
         get
@@ -46,16 +49,11 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    public Player Player { get; set; }
-    public OddsManager OddsManager { get; set; }
-    public GamePhase Phase;
-    public GameDay Day;
-
-    [SerializeField] Button nextGamePhase;
-
     void Start()
     {
         nextGamePhase.onClick.AddListener(MoveToNextPhase);
+        m_winScene.blocksRaycasts = false;
+        m_loseScene.blocksRaycasts = false;
     }
 
     private void Awake()
@@ -70,6 +68,10 @@ public class GameManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
+        m_fadeAnimator = GetComponent<FadeAnimator>();
+        m_winScene.alpha = 0.0f;
+        m_loseScene.alpha = 0.0f;
+        m_gameScene.alpha = 1.0f;
     }
 
     public void MoveToNextPhase()
@@ -86,10 +88,12 @@ public class GameManager : MonoBehaviour
     public void MoveToNextDay()
     {
         Day++;
-        if((int)(Day) == 4)
-        {
-            CheckWinCondition();
-        }
+        CheckWinCondition();
+
+        //if ((int)(Day) == 4)
+        //{
+        //    CheckWinCondition();
+        //}
     }
     
     public void CheckWinCondition()
@@ -98,7 +102,7 @@ public class GameManager : MonoBehaviour
         {
             Win();
         }
-        else
+        if (Player.GetChips() <= 0)
         {
             Lose();
         }
@@ -107,19 +111,31 @@ public class GameManager : MonoBehaviour
 
     void Win()
     {
-        //Stub for winning
+        StartCoroutine(FadeToWin());
         Debug.Log("Win");
     }
 
     void Lose()
     {
-        //Stub for losing
+        StartCoroutine(FadeToLose());
         Debug.Log("Lose");
     }
-
-
-    void Update()
+    IEnumerator FadeToWin()
     {
-        
+        m_gameScene.blocksRaycasts = false;
+        m_loseScene.blocksRaycasts = false;
+        m_fadeAnimator.FadeOut(m_gameScene, 2f);
+        yield return new WaitForSeconds(0.5f);
+        m_fadeAnimator.FadeIn(m_winScene, 1f);
+        m_winScene.blocksRaycasts = true;
+    }
+    IEnumerator FadeToLose()
+    {
+        m_gameScene.blocksRaycasts = false;
+        m_winScene.blocksRaycasts = false;
+        m_fadeAnimator.FadeOut(m_gameScene, 2f);
+        yield return new WaitForSeconds(0.5f);
+        m_fadeAnimator.FadeIn(m_loseScene, 1f);
+        m_loseScene.blocksRaycasts = true;
     }
 }
