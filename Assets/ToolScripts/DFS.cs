50ed6299fc3a7983e3e31fa4767f0275d6226281
William Friis <56981641+KingWille@users.noreply.github.com>
2025-11-13 21:13:36 +0100

    Retrofitted DFS to dialogue structure

--- Assets/ToolScripts/DFS.cs
+++ Assets/ToolScripts/DFS.cs
@@ -1,44 +1,80 @@
 //Author: William Friis
 using UnityEngine;
+using System.Collections.Generic;
+using System.IO;
 
 #nullable enable
-public class TestNode //Class as to be nullable, as well as default ref instead of copy
+
+public class Node //Class as to be nullable, as well as default ref instead of copy
 {
     public bool Visited;
     public string Dialogue;
-    public TestNode[]? Children;
+    public string ID;
+    public int DTreeIndex;
+    public int TreeIndex;
+    public List<Node> Children;
+
+    public Node(string text, string id, int dTreeIndex)
+    {
+        Visited = false;
+        Dialogue = text;
+        ID = id;
+        Children = new List<Node>();
+        DTreeIndex = dTreeIndex;
+    }
 }
 
 public class DFS : MonoBehaviour
 {
     public uint Iterations;
     public string Goal;
-    
-    private TestNode treeRoot;
+
+    public NpcDefinition[] Npcs;
+    public DialogueManager DialogueManager;
+
+    private List<DialogueTree> DialogueTrees;
+    private List<Node> DialogueTreeRoots;
     private void Start()
     {
-        treeRoot = new TestNode { Visited = false, Dialogue = "root" };
+        DialogueTrees = new List<DialogueTree>();
+        DialogueTreeRoots = new List<Node>();
 
-        CreateTestTree(treeRoot, 0, Iterations);
+        InitTrees();
+        BuildTree(DialogueTreeRoots[0], 0);
 
-        TestNode result = Search(treeRoot, Goal);
+        var goal = "Interesting. I see what you want me to do, kid. If you've got the chips, I'll do it.";
+        var result = Search(DialogueTreeRoots[0], goal);
 
         if (result == null)
             Debug.Log("Unable to find goal");
         else
-            Debug.Log($"Goal found at {result.Dialogue}");
+            Debug.Log($"Goal found at level {result.TreeIndex}");
     }
 
-    public TestNode Search(TestNode root, string goal)
+    public void InitTrees()
     {
-        TestNode result;
+        for(int i = 0; i < Npcs.Length; i++)
+        {
+            var path = Path.Combine(Application.streamingAssetsPath, "Dialogues", Npcs[i].dialogueFile);
+
+            var json = File.ReadAllText(path);
+            
+            var tree = DialogueTree.FromJson(json);
+
+            DialogueTrees.Add(tree);
+            DialogueTreeRoots.Add(new Node(tree.GetNode(tree.start[0]).text, tree.start[0], i));
+        }
+    }
+    public Node Search(Node root, string goal)
+    {
+        Node result;
 
         root.Visited = true;
 
         if (root.Dialogue == goal)
             return root;
 
-        for (int i = 0; i < root.Children.Length; i++)
+        for (int i = 0; i < root.Children.Count; i++)
         {
             if (root.Children[i] == null) return null;
 
@@ -52,39 +88,47 @@ public class DFS : MonoBehaviour
 
         return null;
     }
-    public void CreateTestTree(TestNode currNode, int iterationCounter, uint iterationMax)
-    {
-        currNode.Children = new TestNode[2];
-        iterationCounter++;
 
-        if (iterationCounter > iterationMax)
+    public void BuildTree(Node root, int currIndex)
+    {
+        root.TreeIndex = currIndex;
+
+        foreach (var choice in DialogueTrees[root.DTreeIndex].GetNode(root.ID).choices)
         {
-            for (int i = 0; i < currNode.Children.Length; i++)
+            if(!IsEmpty(choice.gotoNode))
             {
-                currNode.Children[i] = null;
+                var newNode = new Node(DialogueTrees[root.DTreeIndex].GetNode(choice.gotoNode).text, choice.gotoNode, root.DTreeIndex);
+
+                root.Children.Add(newNode);
+                BuildTree(newNode, root.TreeIndex + 1);
             }
-        }
-        else
-        {
-            for (int i = 0; i < currNode.Children.Length; i++)
+
+            if (!IsEmpty(choice.gotoOnSuccess))
             {
-                currNode.Children[i] = new TestNode { Visited = false, Dialogue = $"Node level: {iterationCounter}" };
-                CreateTestTree(currNode.Children[i], iterationCounter, iterationMax);
+                var newNode = new Node(DialogueTrees[root.DTreeIndex].GetNode(choice.gotoOnSuccess).text, choice.gotoOnSuccess, root.DTreeIndex);
+
+                root.Children.Add(newNode);
+                BuildTree(newNode, root.TreeIndex + 1);
+            }
+
+            if (!IsEmpty(choice.gotoOnFail))
+            {
+                var newNode = new Node(DialogueTrees[root.DTreeIndex].GetNode(choice.gotoOnFail).text, choice.gotoOnFail, root.DTreeIndex);
+
+                root.Children.Add(newNode);
+                BuildTree(newNode, root.TreeIndex + 1);
             }
         }
     }
 
-    public void Reset(TestNode root, bool isTreeRoot)
+    public bool IsEmpty(string? s)
     {
-        if(!isTreeRoot)
-            root.Visited = false;
+        if(s == null || s == "" || s == string.Empty)
+            return true;
 
-        for(int i = 0; i < root.Children.Length; i++)
-        {
-            if (root.Children[i] != null)
-                Reset(root.Children[i], false);
-        }
+        return false;
     }
-}
+}   
+
 
 
